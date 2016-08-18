905b5b31238cc89b8781334bfb784b8400b9ad96
ronniefeng <ronniefeng@tencent.com>
2016-08-18 11:49:35 +0800

    Init

---
+++ YaKeychain/YaKeychain.h
@@ -0,0 +1,13 @@
+//
+//  YaKeychain.h
+//  YaKeychain
+//
+//  Created by Yeelik on 16/8/18.
+//  Copyright © 2016年 FYL. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YaKeychain : NSObject
+
+@end
---
+++ YaKeychain/YaKeychain.m
@@ -0,0 +1,13 @@
+//
+//  YaKeychain.m
+//  YaKeychain
+//
+//  Created by Yeelik on 16/8/18.
+//  Copyright © 2016年 FYL. All rights reserved.
+//
+
+#import "YaKeychain.h"
+
+@implementation YaKeychain
+
+@end
